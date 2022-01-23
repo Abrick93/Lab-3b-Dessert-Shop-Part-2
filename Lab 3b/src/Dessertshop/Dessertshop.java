c5c337f67445a2810e1bbf7c6e8e9345b21d7402
amand <amand@192.168.1.127>
2022-01-22 19:05:17 -0700

    DessertShop part 2

---
+++ Lab 3b/src/Dessertshop/Dessertshop.java
@@ -0,0 +1,23 @@
+package Dessertshop;
+
+import java.util.ArrayList;
+
+public class Dessertshop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 ArrayList<String> DessertItem = new ArrayList<String>();
+		 
+		 	DessertItem.add("Candy Corn"); 
+		 	DessertItem.add("Gummy Bears");
+		 	DessertItem.add("Chocolate Chip");
+		 	DessertItem.add("Pistachio");
+		 	DessertItem.add("Vanilla");
+		 	DessertItem.add("Oatmeal Raisin");
+	
+		 	System.out.println("Total number of items in order:6");
+		
+	}//end of main()
+
+}//end of Dessert shop
